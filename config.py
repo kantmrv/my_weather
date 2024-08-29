66b339bc292933d724c8cfc8d30702f9d4f3ad9f
Alexandr <123596410+kantmrv@users.noreply.github.com>
2024-08-30 00:00:16 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
 ROUND_COORDINATES: bool = True
 METRIC_UNITS: bool = True
 IMPERIAL_UNITS: bool = False
-OPENWEATHER_API: str = "929a3822e6449489b4154beb6a184037"
+OPENWEATHER_API: str = "YOUR_OPENWEATHER_API_KEY"
 OPENWEATHER_URL: str = "https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={api}&units={units}"
